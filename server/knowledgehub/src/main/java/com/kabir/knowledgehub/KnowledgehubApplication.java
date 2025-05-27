1c7a11fb33112c8655f362ee24d0d7ed2d5d57da
Kabir Sethi <ks19pt@brocku.ca>
2025-05-26 22:31:01 -0400

    Intial commit - full stack ai knowledge hub

---
+++ server/knowledgehub/src/main/java/com/kabir/knowledgehub/KnowledgehubApplication.java
@@ -0,0 +1,11 @@
+package com.kabir.knowledgehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgehubApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(KnowledgehubApplication.class, args);
+	}
+}
